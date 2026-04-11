698cab98151b24514130ffe0401e716e5d99a690
Christian Winkler <christian.winkler@lehrcode.de>
2026-04-11 19:29:40 +0200

    aot processing

--- src/main/java/de/lehrcode/burnafterreading/DataJdbcConfiguration.java
+++ src/main/java/de/lehrcode/burnafterreading/DataJdbcConfiguration.java
@@ -1,9 +1,23 @@
 package de.lehrcode.burnafterreading;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.core.dialect.JdbcDialect;
+import org.springframework.data.jdbc.core.dialect.JdbcPostgresDialect;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
 @Configuration
 @EnableJdbcAuditing
 public class DataJdbcConfiguration {
+    /**
+     * @see "https://github.com/spring-projects/spring-boot/issues/48240"
+     * @see "https://github.com/spring-projects/spring-boot/issues/47781"
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public JdbcDialect jdbcDialect() {
+        return JdbcPostgresDialect.INSTANCE;
+    }
 }
